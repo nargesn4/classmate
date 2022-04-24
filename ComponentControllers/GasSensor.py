cf6a1ab80536a9ea138bc71a1785b93e0ac205b3
Narges Jafari <narges@orangebeard.io>
2022-04-24 14:26:58 +0200

    adding comment to explain the code

--- ComponentControllers/GasSensor.py
+++ ComponentControllers/GasSensor.py
@@ -1,8 +1,13 @@
 from gas_detection import GasDetection
 from time import sleep
 
+#instantiate our GasDetection class.
 smoke_detector = GasDetection()
+#Wait 20 seconds for it to calibrate. This should be done in the fresh air.
 sleep(2)
+#This will return us an object of all gases and their concentrations in the air.
 ppm = smoke_detector.percentage()
+#chekking the smoke
 smoke_value = ppm[smoke_detector.SMOKE_GAS]
+#printing the value
 print(smoke_value)
